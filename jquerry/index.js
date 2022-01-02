92ceabf5fde320fa8e3e559abe48127e963a837b
mariallugare <mariallugare@gmail.com>
2022-01-02 22:19:52 -0100

    work on the hover effect

---
+++ jquerry/index.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+    $(".dev2").click(function(){
+      $(".dev2").toggle();
+        $(".dev").toggle();
+         $(".dev").click(function(){
+          $(".dev").hide();
+          $(".dev2").show();
+         });
+    });
+  });
+
+  $(document).ready(function(){
+    $(".des1").click(function(){
+      $(".des1").toggle();
+        $(".des").toggle();
+         $(".des").click(function(){
+          $(".des").hide();
+          $(".des1").show();
+         });
+    });
+  });
+
+  $(document).ready(function(){
+    $(".pd2").click(function(){
+      $(".pd2").toggle();
+        $(".pd").toggle();
+         $(".pd").click(function(){
+          $(".pd").hide();
+          $(".pd2").show();
+         });
+    });
+  });
+
+
+  $('.work1').mouseover(function () {
+    $('.bs').show();
+  }).mouseout(function () {
+    $('.bs').hide();
+  });
+
+  $('.work2').mouseover(function () {
+    $('.os').show();
+  }).mouseout(function () {
+    $('.os').hide();
+  });
+
+  $('.work3').mouseover(function () {
+    $('.pm').show();
+  }).mouseout(function () {
+    $('.pm').hide();
+  });
+
+  $('.work4').mouseover(function () {
+    $('.jm').show();
+  }).mouseout(function () {
+    $('.jm').hide();
+  });
+
+  $('.work5').mouseover(function () {
+    $('.nd').show();
+  }).mouseout(function () {
+    $('.nd').hide();
+  });
+  $('.work6').mouseover(function () {
+    $('.c').show();
+  }).mouseout(function () {
+    $('.c').hide();
+  });
+  $('.work7').mouseover(function () {
+    $('.b').show();
+  }).mouseout(function () {
+    $('.b').hide();
+  });
+  $('.work8').mouseover(function () {
+    $('.gr').show();
+  }).mouseout(function () {
+    $('.gr').hide();
+  });
+
+
+
+
+
+
